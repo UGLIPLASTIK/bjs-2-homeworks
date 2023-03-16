4885a4eeec6f9d78a54974a720255ae44812a550
Dmitry.Kononets <d.cononec@gmail.com>
2023-03-17 02:30:22 +0300

    задание 2 готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,10 +4,10 @@ function solveEquation(a, b, c) {
   let d = Math.pow(b, 2)-4*a*c;
 
   if (d > 0) {
-    let solOne = (-b + Math.sqrt(d)) / (2*a);
-    let solTwo = (-b - Math.sqrt(d)) / (2*a);
-    arr.push(solOne.toFixed(2));
-    arr.push(solTwo.toFixed(2));
+    let rootOne = (-b + Math.sqrt(d)) / (2*a);
+    let rootTwo = (-b - Math.sqrt(d)) / (2*a);
+    arr.push(rootOne.toFixed(2));
+    arr.push(rootTwo.toFixed(2));
   } else if (d === 0) {
     let root = -b/(2*a);
     arr.push(root);
@@ -20,6 +20,28 @@ let solution = solveEquation(2, 9, 1);
 console.log(solution);
 // solveEquation(16, 8, 1) равен 0
 
+
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  switch (true) {
+    case isNaN(percent):
+      return console.log("Некорректное значение percent")
+    case isNaN(contribution):
+      return console.log("Некорректное значение contribution")
+    case isNaN(amount):
+      return console.log("Некорректное значение amount")
+    case isNaN(countMonths):
+      return console.log("Некорректное значение countMonths")
+  }
+
+  const convertPercent = percent / 100;
+  const mounthPercent = convertPercent / 12;
+  const creditBody = amount - contribution;
+  const monthlyPay = creditBody * (mounthPercent + (mounthPercent / (Math.pow(1 + mounthPercent, countMonths) - 1)));
+  const totalAmount = monthlyPay * countMonths;
+  // console.log(`Ежемесячный платеж: ${monthlyPay.toFixed(2)}`);
+  return totalAmount;
 }
+
+let total = calculateTotalMortgage(10, 0, 50000, 12);
+console.log(`Всего: ${+total.toFixed(2)}`);
