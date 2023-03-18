cdcce2829a4771a8783d85da1eaadce787cc4351
Dmitry.Kononets <d.cononec@gmail.com>
2023-03-19 02:46:23 +0300

    первая задача готова

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,15 @@
-function getArrayParams(...arr) {
+'use strict'
 
-  return { min: min, max: max, avg: avg };
+function getArrayParams(...arr) {
+  let max = Math.max.apply(null, arr),
+      min = Math.min.apply(null, arr),
+      avg = arr.reduce((acc, arr) => acc + arr, 0) / arr.length;
+  
+  return { min: min, max: max, avg: +avg.toFixed(2) };
 }
+
+let sol = getArrayParams(1,2,3,4,50,7,7,8,30);
+console.log(sol)
 
 function summElementsWorker(...arr) {
 
