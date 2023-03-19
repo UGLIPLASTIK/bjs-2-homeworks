1d69f572cd4a1189929918e45ad32a0cda67678c
Dmitry.Kononets <d.cononec@gmail.com>
2023-03-19 17:53:16 +0300

    задача 2 готова

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -12,6 +12,9 @@ let sol = getArrayParams(1,2,3,4,50,7,7,8,30);
 console.log(sol)
 
 function summElementsWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
   const sum = arr.reduce((acc, arr) => acc + arr, 0);
   return sum
 }
@@ -20,6 +23,9 @@ let solSum = summElementsWorker(1,2,3,4,50,7,7,8,30);
 console.log(sum);
 
 function differenceMaxMinWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
   let max = Math.max.apply(null, arr),
       min = Math.min.apply(null, arr);
   const difference = max - min;
@@ -30,39 +36,41 @@ let solDif = differenceMaxMinWorker(1,2,3,4,50,7,7,8,30);
 console.log(solDif);
 
 function differenceEvenOddWorker(...arr) {
-  const sumEvenEl = 0,
-        sumOddEl = 0;
-  for (let i = 0; i > arr.length; i++){
-    if (arr[i] % 2 == 0) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  let sumEvenEl = 0,
+      sumOddEl = 0;
+  for (let i = 0; i < arr.length; i++){
+    if ((arr[i] % 2) === 0) {
       sumEvenEl += arr[i];
     } else {
       sumOddEl += arr[i];
     }
   }
-  console.log(`SumEven ${sumEvenEl}, sumodd ${sumOddEl}`);
-  return sumEvenEl;
+  return sumEvenEl - sumOddEl;
 }
-let solEven = differenceEvenOddWorker(1,2,3,4,50,7,7,8,30);
-console.log(solEven);
+let solEvenDiff = differenceEvenOddWorker(1,2,3,4,5,6,7,8,9);
+console.log(solEvenDiff);
 
-let array = [1,2,3,4];
-let sumEvenEl = 0,
-      sumOddEl = 0;
-for (let i = 0; i > array.length; i++){
-  console.log(array[i]);
-  // if (array[i] % 2 === 0) {
-  //   sumEvenEl += array[i];
-  //   console.log(sumEvenEl);
-  // } else {
-  //   sumOddEl += array[i];
-  //   console.log(sumOddEl);
-  // }
-}
-console.log(sumOddEl);
 
 function averageEvenElementsWorker(...arr) {
-
+  if (arr.length === 0) {
+    return 0;
+  }
+  let sumEvenElements = 0,
+      countEvenElements = 0;
+      for (let i = 0; i < arr.length; i++){
+        if ((arr[i] % 2) === 0) {
+          sumEvenElements += arr[i];
+          countEvenElements++;
+        }
+      }
+  return sumEvenElements / countEvenElements;
 }
+
+let solAverEven = averageEvenElementsWorker(1,2,3,4,5,6,7,8,9);
+console.log(solAverEven);
 
 function makeWork (arrOfArr, func) {
 
