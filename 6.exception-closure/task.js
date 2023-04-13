0decbc7c86b81bcea1d189c64813084648f703c6
Dmitry.Kononets <d.cononec@gmail.com>
2023-04-13 22:32:46 +0300

    Готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,8 +1,9 @@
 ﻿function parseCount(number){
-  if(isNaN(Number.parseFloat(number))) {
+  let parseInt = Number.parseFloat(number)
+  if(isNaN(parseInt)){
     throw new Error("Невалидное значение");
   }
-  return Number.parseFloat(number)
+  return parseInt
 }
 
 function validateCount(number){
@@ -10,7 +11,6 @@ function validateCount(number){
     return parseCount(number);
   } catch (error){
     return error;
-  } finally {
   }
 }
 
@@ -30,20 +30,31 @@ class Triangle{
   }
 
   get area(){
-    const pp = 1/2 * (this.side1 + this.side2 + this.side3);
+    let pp = this.perimeter / 2;
     return Number(Math.sqrt(pp * (pp - this.side1)*(pp - this.side2)*(pp - this.side3)).toFixed(3))
   }
 }
 
 
-function getTriangle(a,b,c){
-  if(a + b < c || b + c < a || a + c < b){
-    throw new Error('Треугольник с такими сторонами не существует');
+function getTriangle(side1,side2,side3){
+  try{
+    if(side1 + side2 < side3 || side2 + side3 < side1 || side1 + side3 < side2){
+      throw error
+    }
+    return new Triangle(side1,side2,side3)
+
+  } catch(error){
+    let triangle = {
+      get perimeter(){
+        return "Ошибка! Треугольник не существует"
+      },
+      get area(){
+        return "Ошибка! Треугольник не существует"
+      }
+    }
+    return triangle
   }
-  this.a = a,
-  this.b = b,
-  this.c = c
 }
 
-let newTriangle = new getTriangle(6,5,15);
-console.log(newTriangle);
+let triangle = getTriangle(6,10,15);
+console.log(triangle);
